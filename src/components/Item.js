a3b42ed71c4ae81ffb4685d32ddae3fd7f99641f
Stéphano Giuseppe <stephano.grp@gmail.com>
2019-07-28 23:54:05 -0300

    add: proptypes and defaultprops example

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 function Item({ item, onDelete }) {
   return <li>
     {item}
     <button type="button" onClick={onDelete}>Remove</button>
   </li>
+}
+
+Item.defaultProps = {
+  item = 'Null'
+}
+
+Item.propTypes = {
+  item = PropTypes.string,
+  onDelete = PropTypes.func.isRequired
 }
 
 export default Item
